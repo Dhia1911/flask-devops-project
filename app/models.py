bd434a14cf74580afc224557a8ef8307241fbfcf
Dhia1911 <148390761+Dhia1911@users.noreply.github.com>
2026-05-17 23:30:49 +0100

    Ajout du fichier models.py

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    price = db.Column(db.Float)
